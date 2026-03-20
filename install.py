ef4809818e7de31eb347767ab4fcb0d60c437677
imngkhang <nnguyenkhang14@gmail.com>
2026-03-20 21:46:09 +0700

    Update install.py

--- install.py
+++ install.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 cur_path = sys.path[0]
 
-os.system("pip3 install -U pip wheel setuptools")
+os.system("pip3 install -U pip wheel setuptools nuitka zstandard")
 os.system("pip3 install -r requirements.txt")
 os.system("mkdir -p ~/.local/share/icons")
 os.system("cp {}/icons/logo.png /usr/share/icons/warp_gui.png".format(cur_path))
+os.system("python3 -m nuitka --standalone --onefile --output-dir=dist --enable-plugin=pyqt5 --include-data-dir=icons=icons --include-data-dir=warp_gui/ui=warp_gui/ui --include-data-dir=designer=designer --include-data-dir=requirements=requirements --include-data-files=requirements.txt=requirements.txt --include-data-files=LICENSE=LICENSE -o warp-qt main.py")
 
 desktop_file = '/usr/share/applications/warp-gui.desktop'.format(Path.home())
 
